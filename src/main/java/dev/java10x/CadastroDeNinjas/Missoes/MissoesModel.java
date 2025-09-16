8b1bb95d095382c169bba46dfd7e5b811401e375
Larissa Menezes <larissasezenem@gmail.com>
2025-09-16 20:46:53 -0300

    feat(missoes): adiciona relação one-to-many com NinjaModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,6 +3,8 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "tb_missoes")
 public class MissoesModel {
@@ -13,5 +15,7 @@ public class MissoesModel {
     private String nome;
     private String dificuldade;
 
-    private NinjaModel ninja;
+    //@OneToMany - Uma missão pode ter vários ninjas
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
 }
