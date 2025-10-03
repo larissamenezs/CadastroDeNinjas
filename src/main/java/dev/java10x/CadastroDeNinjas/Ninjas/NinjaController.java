5a2e21c83e7ef242176e2a7ced3efacdbcbfa7ab
Larissa Menezes <larissasezenem@gmail.com>
2025-10-03 14:06:15 -0300

    feat(ninja): altera a rota do controller para listar os ninjas e injeta a dependência do Service e do Repository

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,4 +1,51 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    //Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    //Mostrar todos os ninjas (READ)
+    @GetMapping("/listar")
+    public List<NinjaModel> listarNinjas() {
+        return ninjaService.listarNinjas();
+    }
+
+    //Mostrar ninja por id (READ)
+    @GetMapping("/listarID")
+    public String mostrarTodosOsNinjasPorId() {
+        return "Mostrar ninja por ID";
+    }
+
+    //Alterar dados dos ninjas (UPDATE)
+    @PostMapping("/alterarID")
+    public String alterarNinjaPorId() {
+        return "Alterar Ninja por ID";
+    }
+
+    //Deletar Ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Ninja deletado por ID";
+    }
 }
